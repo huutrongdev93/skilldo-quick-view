cac7ce585ef46242e2ba151c49850a8d2c0796f2
huutrongdev93 <huutrong.it93@gmail.com>
2024-11-26 14:53:40 +0700

    Fix lỗi load ajax

--- styles/style.script.php
+++ styles/style.script.php
@@ -3,13 +3,8 @@
         $(document).on('click', '.btn-quickview', function (e) {
             e.preventDefault();
             let pid = $(this).attr('data-id');
-            $.ajaxSetup({
-                beforeSend: function(xhr, settings) {
-                    settings.data += '&csrf_test_name=' + encodeURIComponent(getCookie('csrf_cookie_name'));
-                }
-            });
             $.fancybox.open({
-                src  : base + '/ajax?action=QuickViewRender&id=' + pid,
+                src  : ajax + '?action=QuickViewRender&id=' + pid + '&csrf_test_name=' + encodeURIComponent(getCookie('csrf_cookie_name')),
                 type : 'ajax',
                 opts : {
 					touch: false,
